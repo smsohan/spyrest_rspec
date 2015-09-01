6d44162845db5c3482ccca55d06a327a0b13cf66
smsohan <sohan39@gmail.com>
2015-09-01 14:44:52 -0600

    first test working

--- spec/support/fire_amp.rb
+++ spec/support/fire_amp.rb
@@ -8,7 +8,7 @@ class FireAMP
     'content-type' => 'application/json'
   )
 
-  def self.api_key_param
+  def self.common_parameters
     {api_key: ENV['FIREAMP_API_KEY'], client_id: ENV['FIREAMP_CLIENT_ID']}
   end
 
--- spec/versions_spec.rb
+++ spec/versions_spec.rb
@@ -1,5 +1,6 @@
-describe "version" do
-  it 'renders the current API version' do
-    FireAMP.get 'v1/version.json', @common_options
+describe "Version" do
+  it 'Renders the current API version' do
+    response = FireAMP.get '/v1/version.json', @common_options.merge(query: FireAMP.common_parameters)
+    expect(response.status).to eql(200)
   end
 end
