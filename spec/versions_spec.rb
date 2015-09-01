27da7cda389d61916fb853d5f63c37f42f53c0b6
smsohan <sohan39@gmail.com>
2015-09-01 15:34:47 -0600

    fixes the init problem

--- spec/versions_spec.rb
+++ spec/versions_spec.rb
@@ -1,5 +1,8 @@
 describe "Version" do
   it 'Renders the current API version' do
+
+    puts "FireAMP.common_parameters = #{FireAMP.common_parameters}"
+
     response = FireAMP.get '/v1/version.json', @common_options.merge(query: FireAMP.common_parameters)
     expect(response.status).to eql(200)
   end
