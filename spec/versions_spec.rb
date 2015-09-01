ab30f90895fe7b524b8e0af5f1fa056103eaeda9
smsohan <sohan39@gmail.com>
2015-09-01 15:35:39 -0600

    fixes the spec

--- spec/versions_spec.rb
+++ spec/versions_spec.rb
@@ -1,9 +1,6 @@
 describe "Version" do
   it 'Renders the current API version' do
-
-    puts "FireAMP.common_parameters = #{FireAMP.common_parameters}"
-
     response = FireAMP.get '/v1/version.json', @common_options.merge(query: FireAMP.common_parameters)
-    expect(response.status).to eql(200)
+    expect(response.code).to eql(200)
   end
 end
