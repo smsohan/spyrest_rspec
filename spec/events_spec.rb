6190fad388225ae81d06655f8da7263f0ba28ffc
Desmond O'Leary <desolear@cisco.com>
2015-09-02 00:16:25 -0600

    Merge branch 'master' of code.engine.sourcefire.com:desolear/fireamp_api_doc
    # Conflicts:
    #	.gitignore
    
    - Added Event Filters Usage

--- spec/events_spec.rb
+++ spec/events_spec.rb
@@ -1,18 +1,25 @@
 describe 'Event' do
   it 'Renders all the events sorted by timestamp descending' do
-    response = FireAMP.get '/v1/events', @common_options.merge(query: FireAMP.common_parameters)
+    response = FireAMP.get '/v1/events', @common_options.merge(query: FireAMP.common_parameters.merge(limit: 1))
     expect(response.code).to eql(200)
   end
 
-  it 'Filters events by connector guids' do
-    params = {connector_guid:     ['c3774873-3653-4064-900a-a54e805c8ccf'],
-              detection_sha256:   ['275a021bbfb6489e54d471899f7db9d1663fc695ec2fe2a2c4538aabf651fd0f'],
-              application_sha256: ['929d1b2079bde9f6a4d90d25f926b86a44f62c33e9522d3e00bcf3ee06a96492'],
-              start_date:         Date.new(2015, 1, 1),
-              event_type:         1090519054,
-              group_guid: ['e53d5971-ccb7-4f54-99d0-09676a6d2ba5']
-    }
-    response = FireAMP.get '/v1/events', @common_options.merge(query: FireAMP.common_parameters.merge(params))
+  it 'Event Filters Usage' do
+    response = FireAMP.get '/v1/events', @common_options.merge(query: FireAMP.common_parameters.merge(threat_detected_event))
     expect(response.code).to eql(200)
+  end
+
+  def threat_detected_event
+    return @event if @event
+    event_type_id = 1090519054 # Threat Detected
+    event = FireAMPDirect.get('/v1/events', @common_options.merge(query: FireAMP.common_parameters.merge(limit: 1, event_type: [event_type_id])))['data']['items'].first
+
+    @event = {
+        connector_guid:     event['computer']['guid'],
+        detection_sha256:   event['file']['identity']['sha256'],
+        application_sha256: event['network_info']['parent']['identity']['sha256'],
+        start_date:         Date.new(2015, 1, 1),
+        event_type:         [event['event_type_id'].to_i]
+    }
   end
 end
